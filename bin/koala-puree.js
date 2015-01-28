2eea7971b468f1b448d4f3bbe9e2da024f43931b
Wong Shek Hei Felix <shekhei@gmail.com>
2015-01-28 17:03:47 +0800

    fixing mdns using our own fork and making global koala-puree reading local koala-puree

--- bin/koala-puree.js
+++ bin/koala-puree.js
@@ -7,11 +7,16 @@ program.version('0.0.1')
 	.action(function(){
 		// console.log("action is starting");
 		// console.log(process.cwd());
-		var path = require('path').resolve(process.cwd()+'/index.js');
-		// console.log(path);
-		var TestApp = require(path);
-		var App = new TestApp();
-		App.start();
+		var path = require('path').resolve(process.cwd()+'/node_modules/.bin/koala-puree');
+		if ( require('fs').existsSync(path) ) {
+			var child = require('child_process').spawn(path, ["start"]);
+		} else {
+			path = require('path').resolve(process.cwd()+'/index.js');
+			// console.log(path);
+			var TestApp = require(path);
+			var App = new TestApp();
+			App.start();
+		}
 	})
 program.parse(process.argv);
 
