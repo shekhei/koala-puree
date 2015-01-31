a262fc45cd5194128ccc7c0cd17229847072a604
Wong Shek Hei Felix <shekhei@gmail.com>
2015-01-31 13:33:46 +0800

    fixing shell infinite loop

--- bin/koala-puree.js
+++ bin/koala-puree.js
@@ -7,16 +7,15 @@ program.version('0.0.1')
 	.action(function(){
 		// console.log("action is starting");
 		// console.log(process.cwd());
-		var path = require('path').resolve(process.cwd()+'/node_modules/.bin/koala-puree');
-		if ( require('fs').existsSync(path) ) {
-			var child = require('child_process').spawn(path, ["start"]);
-		} else {
+//		if ( require('fs').existsSync(path) ) {
+//			var child = require('child_process').spawn(path, ["start"]);
+//		} else {
 			path = require('path').resolve(process.cwd()+'/index.js');
 			// console.log(path);
 			var TestApp = require(path);
 			var App = new TestApp();
 			App.start();
-		}
+//		}
 	})
 program.parse(process.argv);
 
