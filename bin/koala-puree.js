39de1bc628d4dfe203d4b3cae191b4150049c3e4
Wong Shek Hei Felix <shekhei@gmail.com>
2015-03-01 03:16:14 +0800

    making it run harmony if not found

--- bin/koala-puree.js
+++ bin/koala-puree.js
@@ -1,35 +1,43 @@
 #!/usr/bin/env node --harmony
-var program = require('commander');
+if ( process.execArgv.indexOf('--harmony') < 0) {
+	var spawn = require("child_process").spawn;
+	var child = spawn(process.execPath, ['--harmony'].concat(process.execArgv), {
+	  cwd: require("path").dirname(process.execPath)
+	});
+	child.stdout.pipe(process.stdout);
+	child.stderr.pipe(process.stderr);
+} else {
+	var program = require('commander');
 
-program.version('0.0.1')
-	.command('start')
-	.description('starts a simple server based on your config/server.yml')
-	.action(function(){
-		// console.log("action is starting");
-		// console.log(process.cwd());
-//		if ( require('fs').existsSync(path) ) {
-//			var child = require('child_process').spawn(path, ["start"]);
-//		} else {
+	program.version('0.0.1')
+		.command('start')
+		.description('starts a simple server based on your config/server.yml')
+		.action(function(){
+			// console.log("action is starting");
+			// console.log(process.cwd());
+	//		if ( require('fs').existsSync(path) ) {
+	//			var child = require('child_process').spawn(path, ["start"]);
+	//		} else {
+				path = require('path').resolve(process.cwd()+'/index.js');
+				// console.log(path);
+				var TestApp = require(path);
+				var App = new TestApp();
+				App.start();
+	//		}
+		})
+	program.command('migrate')
+		.description('migrates data')
+		.action(function(){
+			console.log("migrating data now");
+			process.env.MODELSYNC = 1;
 			path = require('path').resolve(process.cwd()+'/index.js');
 			// console.log(path);
 			var TestApp = require(path);
 			var App = new TestApp();
-			App.start();
-//		}
-	})
-program.command('migrate')
-	.description('migrates data')
-	.action(function(){
-		console.log("migrating data now");
-		process.env.MODELSYNC = 1;
-		path = require('path').resolve(process.cwd()+'/index.js');
-		// console.log(path);
-		var TestApp = require(path);
-		var App = new TestApp();
-		App.start(null, true).then(function(){
-			console.log("It is completed");
-			process.exit(0);
-		});
-	})
-program.parse(process.argv);
-
+			App.start(null, true).then(function(){
+				console.log("It is completed");
+				process.exit(0);
+			});
+		})
+	program.parse(process.argv);
+}
