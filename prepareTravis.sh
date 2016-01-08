45ae30ead07207bde6f2a6a3c98edfcc3373c56d
Wong Shek Hei Felix <shekhei@gmail.com>
2016-01-08 12:35:00 +0800

    correcting sed to use -e flag

--- prepareTravis.sh
+++ prepareTravis.sh
@@ -10,7 +10,7 @@ wget "http://orientdb.com/download.php?email=unknown@unknown.com&file=${orientdb
 
 tar xzf "${orientdb}.tar.gz"
 cd ${orientdb}
-sed -i "" 's/\<users\>/\<users>\<user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
+sed -i "" -e 's/\<users\>/\<users>\<user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
 ./bin/server.sh &
 curl -X POST http://root:happy@localhost:2480/database/koala-puree-test-test/plocal
 cd ..
