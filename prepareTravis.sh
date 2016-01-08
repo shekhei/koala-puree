be4e43a0d637ff5d9e95282fb87c8f18c51afd95
Wong Shek Hei Felix <shekhei@gmail.com>
2016-01-08 12:49:33 +0800

    debugging the travis thing

--- prepareTravis.sh
+++ prepareTravis.sh
@@ -10,7 +10,8 @@ wget "http://orientdb.com/download.php?email=unknown@unknown.com&file=${orientdb
 
 tar xzf "${orientdb}.tar.gz"
 cd ${orientdb}
-sed -i"" -e 's/\<users\>/\<users>\<user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
+sed -i"" -e 's/\<users>/\<users>\<user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
+cat config/orientdb-server-config.xml
 ./bin/server.sh &
 sleep 2
 curl -X POST http://root:happy@localhost:2480/database/koala-puree-test-test/plocal
