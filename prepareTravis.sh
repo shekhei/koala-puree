00f1ba9e7e9929c2245511db6904122f31db658d
Wong Shek Hei Felix <shekhei@gmail.com>
2016-01-08 11:49:23 +0800

    moving to prepare travis

--- prepareTravis.sh
+++ prepareTravis.sh
@@ -5,12 +5,12 @@ gcc -v || exit 1
 
 filename="mDNSResponder-561.1.1"
 
-wget "http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz"
-tar xzf "bison-2.7.tar.gz"
-cd bison-2.7
-./configure && make && sudo make install
+#wget "http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz"
+#tar xzf "bison-2.7.tar.gz"
+#cd bison-2.7
+#./configure && make && sudo make install
 
-cd ../
+#cd ../
 
 wget "https://opensource.apple.com/tarballs/mDNSResponder/${filename}.tar.gz"
 ln -s $(which gcc) /usr/bin/cc
@@ -21,3 +21,7 @@ make os=linux
 sudo make os=linux install
 
 cd ../../
+
+cd testApp
+npm link ../
+cd ../
