4f5b5d9ba6d9f4eceb94b72fb47241ff8652acca
Wong Shek Hei Felix <shekhei@gmail.com>
2016-01-08 12:12:48 +0800

    adding orientdb and starting it

--- prepareTravis.sh
+++ prepareTravis.sh
@@ -4,6 +4,14 @@ gcc -v || ln -s $(which gcc-4.8) /usr/bin/gcc
 gcc -v || exit 1
 
 filename="mDNSResponder-561.1.1"
+orientdb="orientdb-community-2.1.9"
+wget "http://orientdb.com/download.php?email=unknown@unknown.com&file=${orientdb}.tar.gz&os=linux"
+
+tar xzf "${orientdb}.tar.gz"
+cd ${orientdb}
+sed -i "" 's/\<users\>/\<users>\<user resources="*" password="happy" name="root"\/>/g' config/orientdb-server-config.xml
+./bin/server.sh &
+cd ..
 
 wget "http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz"
 tar xzf "bison-2.7.tar.gz"
