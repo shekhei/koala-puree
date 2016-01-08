41a58abc4d03f2c9c30971c22bc8d7ed8d233f98
Wong Shek Hei Felix <shekhei@gmail.com>
2016-01-08 16:53:42 +0800

    fixing gulpfile based on eslint

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,13 @@
 var run = require("gulp-run");
 var gulp = require("gulp");
-
+var debug = require("debug")("koala-puree:gulp");
 gulp.task("cleanjsdocs", function(cb){
-	    return require("del")(["docs/jsdoc"], cb);
+    return require("del")(["docs/jsdoc"], cb);
 });
 
 gulp.task("jsdoc", ["cleanjsdocs"], function(){
     return run("./node_modules/.bin/jsdoc  -r -d docs/jsdocs **/*.js").exec(function(){
-    	    console.log(arguments);
+        debug(arguments);
     });
 });
 
